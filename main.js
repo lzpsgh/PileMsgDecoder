dfff6c68d9be48f43ba60e3fe6c6caa3596e3473
lzpsgh <lensaclrtn@gmail.com>
2018-10-17 23:09:32 +0800

    [Feature]将2个Item功能合并，智能识别字符串进行相应操作

--- main.js
+++ main.js
@@ -9,47 +9,37 @@ var xmlhttp = new XMLHttpRequest();
 var testPpileMsg1 = "681c3932333430353335373035443746463332141209190e010b00000916";
 var testPileMsg2 = "6888373234353134383330333132303838313300000000000000000b0231343437303530333534303332343931000000000000000000000000000000000300000003000000090000000900000000000000000000000c0000000c000000000000000000000000000000000000000000000000000000141209040f2f17141209040f300700003101011116";
 
-var decodePileMsg = "桩报文解码"
-var id1 = chrome.contextMenus.create({"title":decodePileMsg,"contexts":[context],"onclick":decodeOnClick});
-//var showDeviceLog = "查看设备日志"
-//var id2 = chrome.contextMenus.create({"title":showDeviceLog,"contexts":[context],"onclick":showOnClick});
+var title = "Gizwits"
+var id1 = chrome.contextMenus.create({"title":title,"contexts":[context],"onclick":onClickEvent});
 
-/*
-// Create one test item for each context type.
-var contexts = ["page","selection","link","editable","image","video",
-                "audio"];
-for (var i = 0; i < contexts.length; i++) {
-  var context = contexts[i];
-  var title = "Test '" + context + "' menu item";
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                       "onclick": genericOnClick});
-  console.log("'" + context + "' item:" + id);
+
+function onClickEvent(info, tab){
+ // 使用window.getSelection().toString()无法获取选中文本,只能改用自带的方法
+ var fullText = info.selectionText.toString();
+
+ //非string判断
+ if(typeof fullText != "string"){
+     console.log("wtf");
+ }
+
+ //类型判断
+ var fLen = fullText.length;
+ if( (fullText.substring(0,2)=="68") && (fullText.substring(fLen-2,fLen)=="16")  ){
+     console.log('桩上送报文');
+     decode(fullText);
+ } else if(fLen < 30){
+     console.log("桩运行编码");
+     tran2Mac(fullText);
+ } else{
+     alert("解析异常，该文本非68开头且长度大于30");
+ }
 }
 
-// Create a parent item and two children.
-var parent = chrome.contextMenus.create({"title": "Test parent item"});
-var child1 = chrome.contextMenus.create(
-  {"title": "Child 1", "parentId": parent, "onclick": genericOnClick});
-var child2 = chrome.contextMenus.create(
-  {"title": "Child 2", "parentId": parent, "onclick": genericOnClick});
-console.log("parent:" + parent + " child1:" + child1 + " child2:" + child2);
-*/
-
-
-function decodeOnClick(info, tab){
-  // 使用window.getSelection().toString()无法获取选中文本,只能改用自带的方法
-  sendRequest(info.selectionText);
+function tran2Mac(pileID){
+  alert("这个功能我们很快就上了！！！");
 }
 
-function showOnClick(info, tab) {
-  // console.log("item " + info.menuItemId + " was clicked");
-  // console.log("info: " + JSON.stringify(info));
-  // console.log("tab: " + JSON.stringify(tab));
-  alert('慢慢开发中');
-}
-
-
-function sendRequest(pileMsg){
+function decode(pileMsg){
   xmlhttp.onreadystatechange = callHandle;
   xmlhttp.open("POST", "http://111.207.235.55/m2m/data/resolve?tdsourcetag=s_pctim_aiomsg", true);
   xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
@@ -77,6 +67,7 @@ function callHandle(){
         var patt = /\{[\s\S]*?\}/m; //贪婪模式非全局匹配
         var patt2 = /(?<=break-word;\"\>)[\s\S]*?(?=\<\/div\>)/m; //备用，正向反向零宽断言
         var resp = responseText.match(patt);
+        //var fmResp = JSON.stringify(resp, null, 2);
         if(isCopyClipboard){
           if(window.clipboardData){
             window.clipboardData.setData("Text", resp);
@@ -88,3 +79,30 @@ function callHandle(){
       }
   }
 }
+
+
+
+/*
+// Create one test item for each context type.
+var contexts = ["page","selection","link","editable","image","video",
+                "audio"];
+for (var i = 0; i < contexts.length; i++) {
+  var context = contexts[i];
+  var title = "Test '" + context + "' menu item";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                       "onclick": genericOnClick});
+  console.log("'" + context + "' item:" + id);
+}
+
+console.log("item " + info.menuItemId + " was clicked");
+console.log("info: " + JSON.stringify(info));
+console.log("tab: " + JSON.stringify(tab));
+
+// Create a parent item and two children.
+var parent = chrome.contextMenus.create({"title": "Test parent item"});
+var child1 = chrome.contextMenus.create(
+  {"title": "Child 1", "parentId": parent, "onclick": genericOnClick});
+var child2 = chrome.contextMenus.create(
+  {"title": "Child 2", "parentId": parent, "onclick": genericOnClick});
+console.log("parent:" + parent + " child1:" + child1 + " child2:" + child2);
+*/
